17a8c29e747a47648e3509807ced9f23e59221f6
mqu <meitongdepaul@gmail.com>
2021-10-10 20:54:17 -0700

    seed data for recipes and tags

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .comments import seed_comments, undo_comments
+from .recipes import seed_recipes, undo_recipes
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,6 +13,7 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_comments()
+    seed_recipes()
     # Add other seed functions here
 
 
@@ -20,4 +22,5 @@ def seed():
 def undo():
     undo_users()
     undo_comments()
+    undo_recipes()
     # Add other undo functions here
