ea1b4c9ce0567690b202d20ea16702b97fdbf3eb
nerdkitty1988 <jamiarnold2284@gmail.com>
2021-10-11 12:17:20 -0500

    Setup route files

---
+++ app/api/recipe_routes.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, jsonify
+from app.models import Recipe, db
+
+
+recipe_routes = Blueprint('recipes', __name__)
+
+@recipe_routes.route('/')
+def recipes():
+    recipes = Recipe.query.all()
+    return {'recipes': [recipe.to_dict() for recipe in recipes]}
+
+
---
+++ app/api/tag_routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, jsonify
+from app.models import Tag, db
+
+
+tag_routes = Blueprint('tags', __name__)
+
+
+@tag_routes.route('/')
+def tags():
+    tags = Tag.query.all()
+    return {'tags': [tag.to_dict() for tag in tags]}
