ac1ca56e499e919a75ec36ea267359c752e1d385
johnny <johnpark1987@gmail.com>
2021-10-11 20:46:47 -0700

    still working on homepage stuff

--- app/api/recipe_routes.py
+++ app/api/recipe_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from app.models import Recipe, db
+from app.models import Recipe, Tag, db
 
 
 recipe_routes = Blueprint('recipes', __name__)
@@ -9,7 +9,8 @@ def recipes():
     recipes = Recipe.query.all()
     return {'recipes': [recipe.to_dict() for recipe in recipes]}
 
-@recipe_routes.route('/<tag>')
-def recipes():
-    recipes = Recipe.query.all()
-    return {'recipes': [recipe.to_dict() for recipe in recipes]}
+@recipe_routes.route(f"/{Tag.name}")
+def tag_recipes(name):
+    tagged_recipes = Recipe.query.join(Tag).filter(Tag.name == name).all()
+    print(tagged_recipes)
+    return {'tagged': [tagged_recipe.to_dict() for tagged_recipe in tagged_recipes]}
