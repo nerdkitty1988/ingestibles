b1cdabee2b04b66492498e51d468b4f6fc0a4e15
nerdkitty1988 <jamiarnold2284@gmail.com>
2021-10-13 18:53:37 -0500

    PROFILE DONE

--- react-app/src/components/Profile/profile.js
+++ react-app/src/components/Profile/profile.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink, useHistory } from "react-router-dom";
-import { updateUser } from "../../store/session";
-import "./profile.css"
+import { updateUser, deleteUser } from "../../store/session";
+import "./profile.css";
 
 function Profile() {
 	const dispatch = useDispatch();
@@ -23,13 +23,12 @@ function Profile() {
 	const [biographyShow, setBiographyShow] = useState(true);
 	const [profilePicShow, setProfilePicShow] = useState(true);
 
-
 	const handleSave = async (e) => {
-        e.preventDefault();
+		e.preventDefault();
 		let updatedUser;
 		if (password === repeatPassword && password) {
-            updatedUser = {
-                id: sessionUser.id,
+			updatedUser = {
+				id: sessionUser.id,
 				username: username,
 				email: email,
 				biography: biography,
@@ -37,8 +36,8 @@ function Profile() {
 				password: password,
 			};
 		} else {
-            updatedUser = {
-                id: sessionUser.id,
+			updatedUser = {
+				id: sessionUser.id,
 				username: username,
 				email: email,
 				biography: biography,
@@ -48,185 +47,230 @@ function Profile() {
 
 		const data = await dispatch(updateUser(updatedUser));
 		if (data.errors) {
-            return data.errors;
+			return data.errors;
 		} else {
-            setUsernameShow(true)
-            setProfilePicShow(true)
-            setEmailShow(true)
-            setBiographyShow(true)
-            history.push(`/users/${data.id}`);
+			setUsernameShow(true);
+			setProfilePicShow(true);
+			setEmailShow(true);
+			setBiographyShow(true);
+			history.push(`/users/${data.id}`);
 		}
 	};
-    useEffect(() => {
-        async function fetchData() {
-            const response = await fetch(`/api/users/${sessionUser.id}`);
-            const responseData = await response.json();
-            setUser(responseData);
-        }
-        fetchData();
-    }, []);
+
+    const handleDelete = async (e) => {
+        e.preventDefault();
+        const userId = sessionUser.id
+        await dispatch(deleteUser(userId));
+    }
+
+	useEffect(() => {
+		async function fetchData() {
+			const response = await fetch(`/api/users/${sessionUser.id}`);
+			const responseData = await response.json();
+			setUser(responseData);
+		}
+		fetchData();
+	}, []);
 
 	if (!sessionUser) return null;
 
 	return (
-        <div className="profileBox">
+		<div className="profileBox">
 			<form>
 				<div>
 					{errors.map((error, ind) => (
 						<div key={ind}>{error}</div>
 					))}
 				</div>
-                <img hidden={!profilePicShow} id="profPic" src={profilePic}/>
-				<button
-					type="button"
-					onClick={(e) => setProfilePicShow(false)}
-					hidden={!profilePicShow}
-				>
-					Edit Profile Picture
-				</button>
-				<input
-					hidden={profilePicShow}
-					className="editProfInput"
-					type="url"
-					defaultValue={profilePic}
-					onChange={(e) => setProfilePic(e.target.value)}
-				/>
-				<button
-					type="button"
-					onClick={(e) => handleSave(e)}
-					hidden={profilePicShow}
-				>
-					Save
-				</button>
-				<button
-					type="button"
-					onClick={(e) => setProfilePicShow(true)}
-					hidden={profilePicShow}
-				>
-					Cancel
-				</button>
-				<p hidden={!usernameShow} id="profUsername">Username: {username}</p>
-				<button
-					type="button"
-					onClick={(e) => setUsernameShow(false)}
-					hidden={!usernameShow}
-				>
-					Edit Username
-				</button>
-				<input
-					hidden={usernameShow}
-					className="editProfInput"
-					type="text"
-					defaultValue={username}
-					onChange={(e) => setUsername(e.target.value)}
-				/>
-				<button
-					type="button"
-					onClick={(e) => handleSave(e)}
-					hidden={usernameShow}
-				>
-					Save
-				</button>
-				<button
-					type="button"
-					onClick={(e) => setUsernameShow(true)}
-					hidden={usernameShow}
-				>
-					Cancel
-				</button>
-				<p hidden={!emailShow} id="profEmail">Email: {email}</p>
-				<button
-					type="button"
-					onClick={(e) => setEmailShow(false)}
-					hidden={!emailShow}
-				>
-					Edit Email
-				</button>
-				<input
-					hidden={emailShow}
-					className="editProfInput"
-					type="email"
-					defaultValue={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<button
-					type="button"
-					onClick={(e) => handleSave(e)}
-					hidden={emailShow}
-				>
-					Save
-				</button>
-				<button
-					type="button"
-					onClick={(e) => setEmailShow(true)}
-					hidden={emailShow}
-				>
-					Cancel
-				</button>
-				<p hidden={!biographyShow} id="profBiography">Bio: {biography}</p>
-				<button
-					type="button"
-					onClick={(e) => setBiographyShow(false)}
-					hidden={!biographyShow}
-				>
-					Edit Biography
-				</button>
-				<input
-					hidden={biographyShow}
-					className="editProfInput"
-					type="text"
-					defaultValue={biography}
-					onChange={(e) => setBiography(e.target.value)}
-				/>
-				<button
-					type="button"
-					onClick={(e) => handleSave(e)}
-					hidden={biographyShow}
-				>
-					Save
-				</button>
-				<button
-					type="button"
-					onClick={(e) => setBiographyShow(true)}
-					hidden={biographyShow}
-				>
-					Cancel
-				</button>
-				<button
-					type="button"
-					onClick={(e) => setPasswordShow(false)}
-					hidden={!passwordShow}
-				>
-					Edit Password
-				</button>
-				<input
-					hidden={passwordShow}
-					className="editProfInput"
-					type="password"
-					defaultValue={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<input
-					hidden={passwordShow}
-					className="editProfInput"
-					type="password"
-					defaultValue={repeatPassword}
-					onChange={(e) => setRepeatPassword(e.target.value)}
-				/>
-				<button
-					type="button"
-					onClick={(e) => handleSave(e)}
-					hidden={passwordShow}
-				>
-					Save
-				</button>
-				<button
-					type="button"
-					onClick={() => setPasswordShow(true)}
-					hidden={passwordShow}
-				>
-					Cancel
-				</button>
+				<div className="profileData">
+					<img
+						hidden={!profilePicShow}
+						id="profPic"
+						src={profilePic}
+					/>
+					<button
+						type="button"
+						onClick={(e) => setProfilePicShow(false)}
+						hidden={!profilePicShow}
+                        className="editButtons"
+					>
+						Edit Profile Picture
+					</button>
+					<input
+						hidden={profilePicShow}
+						className="editProfInput"
+						type="url"
+						defaultValue={profilePic}
+						onChange={(e) => setProfilePic(e.target.value)}
+					/>
+					<button
+						type="button"
+						onClick={(e) => handleSave(e)}
+						hidden={profilePicShow}
+                        className="saveButton"
+					>
+						Save
+					</button>
+					<button
+						type="button"
+						onClick={(e) => setProfilePicShow(true)}
+						hidden={profilePicShow}
+                        className="cancelButton"
+					>
+						Cancel
+					</button>
+				</div>
+				<div className="profileData">
+					<p hidden={!usernameShow} id="profUsername">
+						Username: {username}
+					</p>
+					<button
+						type="button"
+						onClick={(e) => setUsernameShow(false)}
+						hidden={!usernameShow}
+                        className="editButtons"
+					>
+						Edit Username
+					</button>
+					<input
+						hidden={usernameShow}
+						className="editProfInput"
+						type="text"
+						defaultValue={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<button
+						type="button"
+						onClick={(e) => handleSave(e)}
+						hidden={usernameShow}
+                        className="saveButton"
+					>
+						Save
+					</button>
+					<button
+						type="button"
+						onClick={(e) => setUsernameShow(true)}
+						hidden={usernameShow}
+                        className="cancelButton"
+					>
+						Cancel
+					</button>
+				</div>
+				<div className="profileData">
+					<p hidden={!emailShow} id="profEmail">
+						Email: {email}
+					</p>
+					<button
+						type="button"
+						onClick={(e) => setEmailShow(false)}
+						hidden={!emailShow}
+                        className="editButtons"
+					>
+						Edit Email
+					</button>
+					<input
+						hidden={emailShow}
+						className="editProfInput"
+						type="email"
+						defaultValue={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<button
+						type="button"
+						onClick={(e) => handleSave(e)}
+						hidden={emailShow}
+                        className="saveButton"
+					>
+						Save
+					</button>
+					<button
+						type="button"
+						onClick={(e) => setEmailShow(true)}
+						hidden={emailShow}
+                        className="cancelButton"
+					>
+						Cancel
+					</button>
+				</div>
+				<div className="profileData">
+					<p hidden={!biographyShow} id="profBiography">
+						Bio: {biography}
+					</p>
+					<button
+						type="button"
+						onClick={(e) => setBiographyShow(false)}
+						hidden={!biographyShow}
+                        className="editButtons"
+					>
+						Edit Biography
+					</button>
+					<textarea
+						hidden={biographyShow}
+						className="editProfBio"
+						type="text"
+						defaultValue={biography}
+						onChange={(e) => setBiography(e.target.value)}
+					/>
+					<button
+						type="button"
+						onClick={(e) => handleSave(e)}
+						hidden={biographyShow}
+                        className="saveButton"
+					>
+						Save
+					</button>
+					<button
+						type="button"
+						onClick={(e) => setBiographyShow(true)}
+						hidden={biographyShow}
+                        className="cancelButton"
+					>
+						Cancel
+					</button>
+				</div>
+				<div className="passwordButtons">
+					<button
+						type="button"
+						onClick={(e) => setPasswordShow(false)}
+						hidden={!passwordShow}
+                        className="editButtons"
+					>
+						Edit Password
+					</button>
+					<input
+						hidden={passwordShow}
+						className="editProfInput"
+						type="password"
+						defaultValue={password}
+                        placeholder="Password"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<input
+						hidden={passwordShow}
+						className="editProfInput"
+						type="password"
+						defaultValue={repeatPassword}
+                        placeholder="Re-enter Password"
+						onChange={(e) => setRepeatPassword(e.target.value)}
+					/>
+					<button
+						type="button"
+						onClick={(e) => handleSave(e)}
+						hidden={passwordShow}
+                        className="saveButton"
+					>
+						Save
+					</button>
+					<button
+						type="button"
+						onClick={() => setPasswordShow(true)}
+						hidden={passwordShow}
+                        className="cancelButton"
+					>
+						Cancel
+					</button>
+				</div>
+                <button type="button" className="deleteAccount" onClick={(e) => {handleDelete(e)}}>Delete Account</button>
 			</form>
 		</div>
 	);
